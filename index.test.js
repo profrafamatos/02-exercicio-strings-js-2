0ebbf70a6fcecbafcf6192624190cfbb3dda82cc
profrafamatos <rcmm@cesar.school>
2024-10-10 09:18:56 -0300

    Arquivos necessários para realizar o exercício

---
+++ index.test.js
@@ -0,0 +1,9 @@
+//Não faça nada neste arquivo, esqueça que ele existe
+
+const { execSync } = require('child_process');
+
+test('verifica a concatenação correta', () => {
+  const output = execSync('node index.js').toString();
+  const expectedOutput = "Aprender Javascript é divertido!";
+  expect(output.trim()).toBe(expectedOutput);
+});
